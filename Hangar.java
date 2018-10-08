c4b1b5000a46d003255969fbe401974e9e7953c1
wilder <aymard.ph@gmail.com>
2018-10-08 17:31:44 +0200

    Abstract commit

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+
+public static void main(String[] args) {
+		Car car = new Car("Clio", 20);
+		System.out.println(car.doStuff()); 
+
+		Boat boat = new Boat("Titanic", 30);
+		System.out.println(boat.doStuff()); 
+	}
+
+
+}
